f9e72f864033969562b39c242cb9d1e95f9f6131
Gracie Richards <gracier@gmail.com>
2024-09-19 13:26:26 -0400

    trying to fix github link

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -8,7 +8,7 @@ export default function TOC() {
         <li><Link to="/Labs/Lab2">Lab 2</Link></li>
         <li><Link to="/Labs/Lab3">Lab 3</Link></li>
         <li><Link to="/Kanbas">Kanbas</Link></li>
-        <li><a id="wd-github" href="https://github.com/gracierichards/kanbas-react-web-app"></a></li>
+        <li><Link id="wd-github" to="https://github.com/gracierichards/kanbas-react-web-app">Code repository on GitHub</Link></li>
       </ul>
     );
   }
