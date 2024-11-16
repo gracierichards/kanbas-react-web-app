0541656542e747db053f37face1b69c0f87ea2fc
Gracie Richards <gracier@gmail.com>
2024-11-16 10:01:26 -0500

    Fixed piazza problem

--- src/Kanbas/Account/Signup.tsx
+++ src/Kanbas/Account/Signup.tsx
@@ -1,13 +1,28 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import * as client from "./client";
+import { useDispatch } from "react-redux";
+import { setCurrentUser } from "./reducer";
 export default function Signup() {
+  const [user, setUser] = useState<any>({});
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const signup = async () => {
+    console.log("Entered signup function in Signup React component. Calling client side's client.signup(user)")
+    const currentUser = await client.signup(user);
+    dispatch(setCurrentUser(currentUser));
+    navigate("/Kanbas/Account/Profile");
+  };
   return (
     <div id="wd-signup-screen">
-      <h3>Sign up</h3>
-      <input placeholder="username" className="form-control mb-2"/>
-      <input placeholder="password" type="password" className="form-control mb-2"/>
-      <input placeholder="verify password" type="password" className="form-control mb-2"/>
-      <Link to="/Kanbas/Account/Profile" className="btn btn-primary w-100"> Sign up </Link><br />
-      <Link to="/Kanbas/Account/Signin" >Sign in</Link>
+      <h1>Sign up</h1>
+      <input value={user.username} onChange={(e) => {console.log("Setting username to " + e.target.value);
+                                                    setUser({ ...user, username: e.target.value });
+                                                    console.log("User object is now " + JSON.stringify(user, null, 2))}}
+             className="wd-username form-control mb-2" placeholder="username" />
+      <input value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })} type="password"
+             className="wd-password form-control mb-2" placeholder="password" />
+      <button onClick={signup} className="wd-signup-btn btn btn-primary mb-2 w-100"> Sign up </button><br />
+      <Link to="/Kanbas/Account/Signin" className="wd-signin-link">Sign in</Link>
     </div>
 );}
